99045c375f6d2e7ff8c091d441f3e77993b56ed9
Alice Lara <hello@alicelara.ca>
2013-07-19 15:20:43 -0400

    tweaking test file to match the tutorial. browsing projects and navigations

--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -20,6 +20,12 @@ class ProjectFlowsTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Project 2')
     assert page.has_content?('Project 3')
 
+    click_link 'Project 1'
+    # Assert we're on project1's show page
+    assert_equal project_path(project1), current_path
+    # Assert on this page the first h1 has the text project1's title
+    assert find('h1:first').has_content? project1.title
+
   end
    test "navigation" do
    	project1 = FactoryGirl.create(:project, :title => "Project 1")
@@ -38,24 +44,9 @@ class ProjectFlowsTest < ActionDispatch::IntegrationTest
     # Assert the projects nav element is active
     assert_equal "Projects", find('.navbar ul li.active a').text
     # Click a link to the project1's show page
+	end
 
-    click_link 'Project 1'
-    # Assert we're on project1's show page
-    assert_equal project_path(project1), current_path
-    # Assert on this page the first h1 has the text project1's title
-    assert find('h1:first').has_content? project1.title
-  
-  end
 
-  test "navigation" do
-    # Create a project to visit its show page at the end of the test
-    project1 = FactoryGirl.create(:project, :title => "Project 1")
-    project2 = FactoryGirl.create(:project, :title => "Project 2")
-    project3 = FactoryGirl.create(:project, :title => "Project 3")
 
-    # On a project's show page, the Projects nav element should still be active
-    click_link 'Project 1'
-    assert_equal "Projects", find('.navbar ul li.active a').text
-  end
 
 end
